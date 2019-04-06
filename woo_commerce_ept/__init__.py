a5ac4832f32a288820a8ed20abb66379ea4ef49c
jobiols <jorge.obiols@gmail.com>
2019-04-06 13:46:57 -0300

    [ADD] eliminar woo_commerce

--- woo_commerce_ept/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import wordpress_xmlrpc
-from . import python_magic_0_4_11
-from . import img_upload
-from . import woocommerce
-from . import models
-from . import wizard
-
-
--- woo_commerce_ept/models/woo_req_type_ept.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields,models
-
-class woo_req_type_ept(models.Model):
-    _name='woo.req.type.ept'
-    
-    name=fields.Char('Type')
